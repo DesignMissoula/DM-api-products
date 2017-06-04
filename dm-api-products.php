8b7e1f2cfd5f6b1dd5f779d2238c670ff0a1b366
Bradford Knowlton <brad@ucanbonline.net>
2017-06-03 20:39:06 -0400

    Initial Commit of settings

---
+++ dm-api-products.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: DM API Products
+Plugin URI: http://designmissoula.com/
+Description: This is not just a plugin, it symbolizes the hope of every page.
+Author: Bradford Knowlton
+Version: 1.0.1
+Author URI: http://bradknowlton.com/
+*/
+
+// Initialize Settings
+require_once(sprintf("%s/inc/settings.php", dirname(__FILE__)));
+$WP_Plugin_Template_Settings = new WP_Plugin_Template_Settings();
---
+++ inc/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+ if(!class_exists('WP_Plugin_Template_Settings'))
+    {
+        class WP_Plugin_Template_Settings
+        {
+            
+            // Settings related functions go here
+            
+        } // END class WP_Plugin_Template_Settings
+    } // END if(!class_exists('WP_Plugin_Template_Settings'))
